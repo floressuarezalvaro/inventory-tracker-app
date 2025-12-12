6c0f188f48ee26ec95c6b8847ba26822e257c148
Alvy <floressuarezalvaro@gmail.com>
2025-12-11 22:18:05 -0500

    First init

---
+++ backend/src/test/java/com/alvy/inventory/InventoryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvy.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
